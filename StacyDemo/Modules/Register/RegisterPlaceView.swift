befdcc707242e6128fb43b85aa69016b6f91d785
Kristijan Kralj <kristijan.kralj@live.com>
2020-12-11 07:31:12 +0100

    EO S5_L3

---
+++ StacyDemo/Modules/Register/RegisterPlaceView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterPlaceView.swift
+//  StacyDemo
+//
+//  Created by Kristijan Kralj on 10/12/2020.
+//
+
+import SwiftUI
+
+struct RegisterPlaceView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RegisterPlaceView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterPlaceView()
+    }
+}
--- StacyDemo/Modules/Register/RegisterTypeView.swift
+++ StacyDemo/Modules/Register/RegisterTypeView.swift
@@ -8,9 +8,76 @@
 import SwiftUI
 
 struct RegisterTypeView: View {
+  
+  let gridColumns = [
+    GridItem(.flexible()),
+    GridItem(.flexible())
+  ]
+  
     var body: some View {
-        Text("Register")
+        
+      NavigationView {
+        VStack{
+          ScreenTitle("What are you looking for?")
+          LazyVGrid(columns: gridColumns, alignment: .center, spacing: 24) {
+            RegisterTypeCard(gradientStartColor: Color.roomStart,
+                             gradientEndColor: Color.roomEnd,
+                             image: "ic_room",
+                             cardText: "Room")
+
+            RegisterTypeCard(gradientStartColor: Color.placeStart,
+                             gradientEndColor: Color.placeEnd,
+                             image: "ic_entire_place",
+                             cardText: "Entire Place")
+            
+            RegisterTypeCard(gradientStartColor: Color.roommateStart,
+                             gradientEndColor: Color.roommateEnd,
+                             image: "ic_roommate",
+                             cardText: "Roommate")
+            
+            RegisterTypeCard(gradientStartColor: Color.tenantStart,
+                             gradientEndColor: Color.tenantEnd,
+                             image: "ic_tenant",
+                             cardText: "Tenant")
+          }
+          .frame(maxWidth: .infinity)
+          .padding()
+          .navigationTitle("Step 1-4")
+        }
+        .frame(maxHeight: .infinity)
+      }
     }
+}
+
+fileprivate struct RegisterTypeCard: View {
+  
+  let gradientStartColor: Color
+  let gradientEndColor: Color
+  let image: String
+  let cardText: String
+  
+  var body: some View {
+    NavigationLink(destination: RegisterPlaceView()) {
+      ZStack {
+        Rectangle()
+          .fill(LinearGradient(gradient: Gradient(colors: [gradientStartColor, gradientEndColor]),
+                               startPoint: .leading,
+                               endPoint: .bottom))
+          .frame(width: 150, height: 150)
+          .cornerRadius(6)
+        Image(image)
+          .resizable()
+          .foregroundColor(.white)
+          .frame(width: 60, height: 60)
+          .offset(y: -20)
+        Text(cardText)
+          .foregroundColor(.white)
+          .bold()
+          .offset(y: 35)
+      }
+      .shadow(radius: 10)
+    }
+  }
 }
 
 struct RegisterTypeView_Previews: PreviewProvider {
