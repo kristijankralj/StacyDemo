37f09b15dddff84891cb93d9f658e6fe4e46a5dc
Kristijan Kralj <kristijan.kralj@live.com>
2020-12-11 09:30:41 +0100

    EO S5_L5

---
+++ StacyDemo/Modules/Register/RegisterMoveDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterMoveDetailsView.swift
+//  StacyDemo
+//
+//  Created by Kristijan Kralj on 11/12/2020.
+//
+
+import SwiftUI
+
+struct RegisterMoveDetailsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct RegisterMoveDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterMoveDetailsView()
+    }
+}
---
+++ StacyDemo/Modules/Register/RegisterProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterProfileView.swift
+//  StacyDemo
+//
+//  Created by Kristijan Kralj on 11/12/2020.
+//
+
+import SwiftUI
+
+struct RegisterProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterProfileView()
+    }
+}
