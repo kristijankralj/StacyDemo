054b7daacc9e3f501b227c0b3eee319e572ec266
Kristijan Kralj <kristijan.kralj@live.com>
2020-12-12 12:21:53 +0100

    EO S5_L9

--- StacyDemo/Modules/Register/RegisterProfileView.swift
+++ StacyDemo/Modules/Register/RegisterProfileView.swift
@@ -8,8 +8,41 @@
 import SwiftUI
 
 struct RegisterProfileView: View {
+  @State var fullName = ""
+  @State var phoneNumber = ""
+  @State var selectedGender = 0
+  
+  let gender = ["Male", "Female", "Other"]
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ScrollView {
+        VStack(spacing: 16) {
+          TextField("Full Name", text: $fullName)
+            .disableAutocorrection(true)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+          
+          TextField("Phone Number", text: $phoneNumber)
+            .disableAutocorrection(true)
+            .keyboardType(.phonePad)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+          
+          VStack(alignment: .leading) {
+            Text("Gender")
+              .foregroundColor(.text)
+            
+            Picker(selection: $selectedGender,
+                   label: Text("Gender")) {
+              ForEach(0..<gender.count) { index in
+                Text(gender[index]).tag(index)
+              }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .background(Color.pickerForeground)
+            .cornerRadius(8)
+          }
+        }
+        .padding()
+      }
     }
 }
 
